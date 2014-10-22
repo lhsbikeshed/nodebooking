f3f17c332862c99ff47896a5e42e826867c2bf91
Charles Yarnold <charlesyarnold@tormentedflame.com>
2014-10-22 08:33:18 +0100

    add twilio support

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
+var cred = require('./credentials.js');
+var client = require('twilio')(cred.accountSID, cred.authToken);
 
 var routes = require('./routes/index');
 //var users = require('./routes/users');
@@ -50,6 +52,36 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 //app.use('/users', users);
+
+function callCrew(systemNumber, bookingNumber, crewNumber){
+  client.makeCall({
+
+      to: bookingNumber, // Any number Twilio can call
+      from: systemNumber, // A number you bought from Twilio and can use for outbound communication
+      url: 'http://twimlets.com/echo?Twiml=%3CResponse%3E%0A%20%20%20%20%3CSay%3EDialing%20crew%20now%2C%20standby%3C%2FSay%3E%0A%20%20%20%20%3CDial%3E' + crewNumber + '%3C%2FDial%3E%0A%3C%2FResponse%3E&' // A URL that produces an XML document (TwiML) which contains instructions for the call
+
+  }, function(err, responseData) {
+
+      //executed when the call has been initiated.
+      if(err){
+        console.log('Error!');
+        console.log(err);
+      }
+      else {
+        console.log(responseData.from); // outputs "+14506667788"
+      }
+
+  });
+}
+
+function formatTelephoneNumber(number){
+  if(number.charAt(0)=='0'){
+    return '+44' + number.substring(1, number.length);
+  }
+  else{
+    return number;
+  }
+}
 
 io.on('connection', function(socket){
   console.log('a user connected');
@@ -132,6 +164,46 @@ io.on('connection', function(socket){
         console.log('some feilds missing');
         socket.emit('teamAddedFail', 'Missing input');
       }
+    }
+  });
+
+  socket.on('patchCall', function (team){
+    if(team._id==''){
+      console.log('error,no team');
+      socket.emit('patchCallFailed', 'No Team ID');
+    }
+    else{
+      console.log('patching team:');
+      console.log(team._id);
+      Booking.findById(team._id, function (err, team){
+        if(err){
+          console.log(err);
+          socket.emit('patchCallFail', err);
+        }
+        else{
+          socket.emit('patchCallSuccess', team);
+          console.log('iniating patch for:');
+          console.log(team._id);
+          client.makeCall({
+
+              to: '+447733223902', // Any number Twilio can call
+              from: cred.systemNumber, // A number you bought from Twilio and can use for outbound communication
+              url: 'http://twimlets.com/echo?Twiml=%3CResponse%3E%0A%20%20%20%20%3CGather%20timeout%3D%2210%22%20numDigits%3D%221%22%20action%3D%22http%3A%2F%2Ftwimlets.com%2Fecho%3FTwiml%3D%253CResponse%253E%250A%2520%2520%2520%2520%253CSay%253EDialing%2520crew%2520now%252C%2520standby%253C%252FSay%253E%250A%2520%2520%2520%2520%253CDial%253E'+encodeURIComponent(formatTelephoneNumber(team.contactNumber))+'%253C%252FDial%253E%250A%253C%252FResponse%253E%22%3E%0A%3CPause%20length%3D%222%22%2F%3E%20%20%20%20%20%20%20%20%3CSay%3ECall%20will%20be%20patched%20to%20crew%20if%20you%20press%20any%20key%3C%2FSay%3E%0A%20%20%20%20%3C%2FGather%3E%0A%3C%2FResponse%3E&'
+
+          }, function(err, responseData) {
+
+              //executed when the call has been initiated.
+              if(err){
+                console.log('Error!');
+                console.log(err);
+              }
+              else {
+                console.log('Call placed'); // outputs "+14506667788"
+              }
+
+          });
+        }
+      });
     }
   });
 });
